4a5d010c0f26725e5c3c083d224440fc369962a9
SonuKM <sonumehta0983@gmail.com>
2025-03-08 01:15:56 +0530

    Create Recipe model with schema

---
+++ backend/models/recipeModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    ingredients: {
+      type: [String],
+      required: [true, "Ingredients are required"],
+    },
+    instructions: {
+      type: String,
+      required: [true, "Instructions are required"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      enum: [
+        "Indian",
+        "Italian",
+        "Chinese",
+        "Mexican",
+        "American",
+        "Mediterranean",
+        "French",
+        "Thai",
+        "Vegetarian",
+        "Dessert",
+        "Vegan",
+        "Breakfast",
+        "Snacks",
+      ], // Restrict categories to predefined values
+    },
+    imageUrl: {
+      type: String,
+    },
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
