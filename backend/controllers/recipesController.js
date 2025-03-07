78f4cb4ca78d63d47953aaaa33166530ba302def
SonuKM <sonumehta0983@gmail.com>
2025-03-08 02:09:07 +0530

    Create POST API to create a new recipe in DB

--- backend/controllers/recipesController.js
+++ backend/controllers/recipesController.js
@@ -1,4 +1,20 @@
 import recipeModel from "../models/recipeModel.js";
 
 // ======== CREATE RECIPE ======
-export const createRecipeController = async (req, res, next) => {};
+export const createRecipeController = async (req, res, next) => {
+  try {
+    const { title, ingredients, instructions, categories } = req.body;
+
+    if (!title || !ingredients || !instructions || !categories) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // req.body.author = req.user.userId;
+    const newRecipe = await recipeModel.create(req.body);
+
+    res.status(201).json({ newRecipe });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error creating recipe" });
+  }
+};
