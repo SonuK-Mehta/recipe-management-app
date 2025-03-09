3fe30720e1a3516abb55683ccc606ed2a3b67af2
SonuKM <sonumehta0983@gmail.com>
2025-03-09 07:45:04 +0530

    Improve Recipe Schema

--- backend/models/recipeModel.js
+++ backend/models/recipeModel.js
@@ -5,32 +5,30 @@ const recipeSchema = new mongoose.Schema(
     title: {
       type: String,
       required: [true, "Title is required"],
+      trim: true,
+      minlength: [3, "Title must be at least 3 characters long"],
     },
     ingredients: {
       type: [String],
       required: [true, "Ingredients are required"],
+      validate: {
+        validator: (value) => value.length > 0,
+        message: "At least one ingredient is required",
+      },
     },
     instructions: {
       type: String,
       required: [true, "Instructions are required"],
+      minlength: [10, "Instructions must be at least 10 characters long"],
     },
     categories: {
       type: String,
       required: [true, "Category is required"],
-      enum: [
-        "Indian",
-        "Italian",
-        "Chinese",
-        "American",
-        "Vegetarian",
-        "Dessert",
-        "Breakfast",
-        "Snacks",
-        "Drinks",
-      ], // Restrict categories to predefined values
+      enum: ["Breakfast", "Lunch", "Dinner", "Snacks", "Dessert", "Drinks"],
     },
     imageUrl: {
       type: String,
+      default: "https://example.com/default-recipe.jpg", // Set a default image
     },
   },
   { timestamps: true }
